dbe86e045216a2d69df733bc533489ea5b12d3f6
Rishitha Minol <rishithaminol@gmail.com>
2018-06-10 01:15:59 +0530

    Geoip location fetching capability added

--- router.js
+++ router.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const maxmind = require('maxmind');
+const csvParser = require('csv-load-sync');
+
+const csv_file = 'country_iso_prefix.csv'
+var csv_table = csvParser(csv_file);
+
+var maxmind_options = {
+  watchForUpdates: true,
+  watchForUpdateNonPersistent: true,
+  watchForUpdatesHook: () => {console.log('GeoipLocation database Updated!');}
+};
+var CountryLookup = maxmind.openSync('./GeoLite2-Country.mmdb', maxmind_options);
 
 // Get balance
 router.get('/balance/:userId', function(req, res, next){
@@ -10,6 +22,25 @@ router.get('/balance/:userId', function(req, res, next){
   } else {
     res.status(200);
     res.send(req.params);
+  }
+});
+
+// Get geolocation information
+router.get('/ipcountry', async function(req, res, next){
+  var client_ip_addr = req.connection.remoteAddress.split(':').pop();
+  var country = CountryLookup.get(client_ip_addr);
+  var iso_code = country['country']['iso_code'];
+  var name = country['country']['names']['en'];
+
+  var i;
+  for (i = 0; i < csv_table.length; i++) {
+    if (csv_table[i]['iso'] === iso_code) {
+      var phone_code = csv_table[i]['phonecode'];
+      console.log({prefix: phone_code, iso_code: iso_code, name: name});
+
+      res.status(200);
+      res.json({prefix: phone_code, iso_code: iso_code, name: name});
+    }
   }
 });
 
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(function(req, res, next){
 });
 
 app.use('/', routes);
+
 // determine all undefined calls as errors (catch 404s)
 app.use(function(req, res, next){
   var err = new Error("Orangepad Systems pvt Ltd. Undefined error");
@@ -16,13 +17,17 @@ app.use(function(req, res, next){
   next(err);
 });
 app.use(function (err, req, res, next) {
-  console.log(req.headers);
-  console.log("[warning] undefined url: {ip_addr: " + req.get('referer') + "}");
+  var client_ip_addr = req.connection.remoteAddress.split(':').pop();
+  var client_http_headers = JSON.stringify(req.headers);
+
+  var db_query = {ip_addr: client_ip_addr, http_headers: client_http_headers};
+  console.log(db_query);
+
   res.status(err.status);
   res.json(err.message);
 })
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
