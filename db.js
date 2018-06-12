3a73baf6cfb2feb31b7e78d3262c9bca9be0f2cf
Rishitha Minol <rishithaminol@gmail.com>
2018-06-12 20:53:21 +0530

    Database connectivity error handling

--- db.js
+++ db.js
@@ -2,11 +2,30 @@ var mysql = require('mysql');
 const fs = require('fs');
 
 var dbconf = JSON.parse(fs.readFileSync('./dbconfig.json', 'UTF-8'));
-var con = mysql.createConnection(dbconf);
+var connection;
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Mysql Database Connected!");
-});
+function handle_disconnect() {
+  connection = mysql.createConnection(dbconf);
 
-module.exports = con;
+  connection.connect(function(err){
+    if (err) {
+      console.log('Error when connecting to the database:', err);
+      setTimeout(handle_disconnect, 2000);
+    } else {
+      console.log('Database connected!');
+    }
+  });
+
+  connection.on('error', function(err){
+    console.log('database error:', err);
+    if (err.code == 'PROTOCOL_CONNECTION_LOST') {
+      handle_disconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handle_disconnect();
+
+module.exports = connection;
