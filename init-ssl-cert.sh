c52d941b25ce2a1c9c306f686b18cc80f21dad59
Rishitha Minol <rishithaminol@gmail.com>
2018-06-14 03:59:30 +0000

    typing mistake fix

--- init-ssl-cert.sh
+++ init-ssl-cert.sh
@@ -5,11 +5,11 @@ if [ "$#" -eq 0 ]; then
 	exit 1
 fi
 
-mkdri -p static/.well-known/acme-challenge
+mkdir -p static/.well-known/acme-challenge
 mkdir -p cert
 certbot --config-dir ./cert --work-dir ./cert --logs-dir ./cert certonly \
 		--webroot -w ./static -d "$1"
 
-ln -sv ./cert/live/$1/fullchain.pem .
-ln -sv ./cert/live/$1/privkey.pem .
+ln -svf ./cert/live/$1/fullchain.pem .
+ln -svf ./cert/live/$1/privkey.pem .
 
